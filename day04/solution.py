d571c8a735dc486619d6a6ef9f79efb417996ea8
John Sutherland <john@starstock.com>
2020-12-07 12:23:01 +0000

    Day 4, part II

--- day04/solution.py
+++ day04/solution.py
@@ -1,3 +1,6 @@
+import re
+
+
 """
 byr (Birth Year)
 iyr (Issue Year)
@@ -40,6 +43,61 @@ def part1(fh):
     return len([1 for passport in parse_file(fh) if valid_passport(passport)])
 
 
+def validate_in_set(value, s):
+    return value in s
+
+
+def validate_regex(value, r):
+    return re.match(r, value)
+
+
+def validate_range(value, mn, mx):
+    return validate_in_set(int(value), range(mn, mx + 1))
+
+
+def validate_height(value):
+    if validate_regex(value, r"[0-9]+in$"):
+        return validate_range(int(value[:-2]), 59, 76)
+    if validate_regex(value, r"[0-9]+cm$"):
+        return validate_range(int(value[:-2]), 150, 193)
+    return False
+
+
+def valid_passport2(passport):
+    """
+    byr (Birth Year) - four digits; at least 1920 and at most 2002.
+    iyr (Issue Year) - four digits; at least 2010 and at most 2020.
+    eyr (Expiration Year) - four digits; at least 2020 and at most 2030.
+    hgt (Height) - a number followed by either cm or in:
+        If cm, the number must be at least 150 and at most 193.
+        If in, the number must be at least 59 and at most 76.
+    hcl (Hair Color) - a # followed by exactly six characters 0-9 or a-f.
+    ecl (Eye Color) - exactly one of: amb blu brn gry grn hzl oth.
+    pid (Passport ID) - a nine-digit number, including leading zeroes.
+    cid (Country ID) - ignored, missing or not.
+    """
+
+    if not valid_passport(passport):
+        return False
+
+    return all([
+        validate_range(passport["byr"], 1920, 2002),
+        validate_range(passport["iyr"], 2010, 2020),
+        validate_range(passport["eyr"], 2020, 2030),
+        validate_height(passport["hgt"]),
+        validate_regex(passport["hcl"], r"^#[0-9a-f]{6}$"),
+        validate_in_set(passport["ecl"], set(["amb", "blu", "brn", "gry", "grn", "hzl", "oth"])),
+        validate_regex(passport["pid"], r"^[0-9]{9}$"),
+    ])
+
+
+def part2(fh):
+    return len([1 for passport in parse_file(fh) if valid_passport2(passport)])
+
+
 if __name__ == "__main__":
     with open("./day04/input.txt") as fh:
-        print(part1(fh))
+        lines = fh.readlines()
+
+        print(part1(lines))
+        print(part2(lines))
