478177bfbcf54c3f0d3b8b85f23230230cd60445
John Sutherland <john@starstock.com>
2020-12-08 11:53:13 +0000

    Day 5, part II

--- day05/solution.py
+++ day05/solution.py
@@ -16,8 +16,18 @@ def part1(lines):
     return max([parse_seat_code(line)[2] for line in lines])
 
 
+def part2(lines):
+    seats = set(parse_seat_code(line)[2] for line in lines)
+
+    mn, mx = min(seats), max(seats)
+
+    # Flight is full, so only 1 missing seat
+    return list(set(range(mn, mx + 1)) - seats)[0]
+
+
 if __name__ == "__main__":
     with open("./day05/input.txt") as fh:
         lines = fh.readlines()
 
         print(part1(lines))
+        print(part2(lines))
